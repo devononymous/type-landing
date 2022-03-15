7abe6589cdc0224f193614ff72b42f546971c55a
devononymous <suloksushil@gmail.com>
2022-03-15 08:18:50 +0530

    'typescript-react-landing-errors'

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import images from './speed.jpg';
 import './App.css';
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  makeStyles,
+} from "@material-ui";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="container">
+     <img src={require('./images/speed.jpg')}   alt="Speed" className="img_setter" />
+
+
     </div>
-  );
+    
+
+  )
 }
 
 export default App;
